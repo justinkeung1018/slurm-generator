386985ff3fbeee51ca2f098101139419f0673de5
justinkeung1018 <justinkeung1018@gmail.com>
2023-12-02 12:38:35 +0000

    Add input error handling, retrieve number of packets

--- main.py
+++ main.py
@@ -8,24 +8,57 @@ import aioconsole
 import aiohttp
 import asyncio
 
-async def main():
-    source_set_name = await aioconsole.ainput("Which question set do you want the SLURM tossups from? ")
-    source_packet_number = int(await aioconsole.ainput("Which packet number do you want? "))
 
+async def main():
     async with aiohttp.ClientSession() as session:
         query_service = await QBReaderQueryService.create(session)
-        tossups = await query_service.packet_tossups(source_set_name, source_packet_number)
 
-        translator = TossupTranslator(GoogleTranslator(session))
-        num_translations = int(await aioconsole.ainput("How many times do you want the tossups to be translated? "))
+        ##################################################
+        ##########      USER PROMPTS BEGIN      ##########
+        ##################################################
+
+        retrieved_num_packets = False
+        while not retrieved_num_packets:
+            set_name = await aioconsole.ainput("Which question set do you want the SLURM tossups from? ")
+            try:
+                num_packets = await query_service.num_packets(set_name)
+                retrieved_num_packets = True
+            except ValueError:
+                await aioconsole.aprint("ERROR: Invalid set name. ", end="")
+
+        retrieved_packet_number = False
+        while not retrieved_packet_number:
+            try:
+                packet_number = int(await aioconsole.ainput(f"{set_name} has {num_packets} packets. Which packet number do you want? "))
+            except ValueError:
+                await aioconsole.aprint("ERROR: Please enter a number. ", end="")
+                continue
+            try:
+                tossups = await query_service.packet_tossups(set_name, packet_number)
+                retrieved_packet_number = True
+            except ValueError:
+                await aioconsole.aprint("ERROR: Packet number out of bounds. ", end="")
+
+        retrieved_num_translations = False
+        while not retrieved_num_translations:
+            try:
+                num_translations = int(await aioconsole.ainput("How many times do you want the tossups to be translated? "))
+                retrieved_num_translations = True
+            except ValueError:
+                await aioconsole.aprint("ERROR: Please enter a number. ", end="")
+
         packet_name = await aioconsole.ainput("Give your SLURM packet a name: ")
 
-        translated_tossups = []
-        for tossup in tossups:
-            translated_tossup = await translator.translate_tossup(tossup, num_translations, Language.ENGLISH, Language.ENGLISH)
-            translated_tossups.append(translated_tossup)
+        ##################################################
+        ###########      USER PROMPTS END      ###########
+        ##################################################
+
+        translator = TossupTranslator(GoogleTranslator(session))
+        translated_tossups = [await translator.translate_tossup(tossup, num_translations, Language.ENGLISH, Language.ENGLISH) for tossup in tossups]
     
     writer = MicrosoftWordWriter()
     writer.write_tossups(packet_name, translated_tossups, packet_name)
 
-asyncio.run(main())
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- qbreader_query_service.py
+++ qbreader_query_service.py
@@ -19,6 +19,7 @@ class QBReaderQueryService:
         return self
 
     async def packet_tossups(self: Self, set_name: str, packet_number: int) -> List[Tossup]:
+        """ Retrieves the list of tossups from the given set and packet. """
         def qbreader_tossup_to_our_tossup(qbr_tossup: qbreader.Tossup) -> Tossup:
             return Tossup(
                 question=qbr_tossup.question,
@@ -28,3 +29,7 @@ class QBReaderQueryService:
                 subcategory=qbr_tossup.subcategory
             )
         return list(map(qbreader_tossup_to_our_tossup, await self.client.packet_tossups(set_name, packet_number)))
+
+    async def num_packets(self: Self, set_name: str) -> int:
+        """ Retrieves the number of packets in the set. """
+        return await self.client.num_packets(set_name)
