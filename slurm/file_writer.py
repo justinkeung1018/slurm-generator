9ac61784d246b164dcbd857961bba5c73546f93d
justinkeung1018 <justinkeung1018@gmail.com>
2023-12-03 19:18:13 +0000

    Update Microsoft Word writer style

--- slurm/file_writer.py
+++ slurm/file_writer.py
@@ -1,6 +1,9 @@
-from slurm.types import Tossup
+from slurm.types import Category, Subcategory, Tossup
 
 from docx import Document
+from docx.enum.style import WD_STYLE_TYPE
+from docx.enum.text import WD_ALIGN_PARAGRAPH
+from docx.shared import Pt
 from docx.text.paragraph import Paragraph
 from html.parser import HTMLParser
 from typing import List, Optional, Self
@@ -29,14 +32,31 @@ class MicrosoftWordWriter(FileWriter):
         basename: Optional[str]=None
     ) -> None:
         document = Document()
-        document.add_heading(packet_name, 0)
+
+        self.set_styles(document)
+
+        self.write_heading(document, packet_name)
 
         for tossup in tossups:
             self.write_question(document, tossup.question)
             self.write_answerline(document, tossup.formatted_answer)
-        
+            self.write_category_and_subcategory(document, tossup.category, tossup.subcategory)
+            self.write_empty_line(document)
+
         document.save(f"{basename if basename else packet_name}.docx")
-    
+
+    def set_styles(self: Self, document: Document) -> None:
+        document.styles["Normal"].font.name = "Arial"
+
+        style = document.styles.add_style('Packet Name', WD_STYLE_TYPE.PARAGRAPH)
+        style.font.name = "Arial"
+        style.font.size = Pt(18)
+        style.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
+        style.paragraph_format.space_after = Pt(16)
+
+    def write_heading(self: Self, document: Document, heading: str) -> None:
+        document.add_paragraph(heading, style="Packet Name")
+
     def write_question(self: Self, document: Document, question: str) -> None:
         document.add_paragraph(question, style="List Number")
 
@@ -44,6 +64,12 @@ class MicrosoftWordWriter(FileWriter):
         paragraph = document.add_paragraph("ANSWER: ", style="List Continue")
         parser = self.MicrosoftWordAnswerLineParser(paragraph)
         parser.feed(answerline)
+
+    def write_category_and_subcategory(self: Self, document: Document, category: Category, subcategory: Subcategory):
+        document.add_paragraph(f"<{category} / {subcategory}>", style="List Continue")
+
+    def write_empty_line(self: Self, document: Document):
+        document.add_paragraph("", style="List Continue")
 
     class MicrosoftWordAnswerLineParser(HTMLParser):
         def __init__(self: Self, paragraph: Paragraph):
